0b2fcbf6c4ff8070a9cafe0fe0e55db753ff38fb
ROHITH KUMAR R <kumarrohith791@gmail.com>
2022-07-14 10:18:55 +0530

    first commit

---
+++ src/Components/Screens/PrivatePages/Shortner.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect } from "react";
+import { useHistory } from "react-router";
+import { userContext } from "../../Context/AuthProvider";
+import ShortnerForm from "./ShortnerForm";
+function Shortner() {
+  const { loggedIn, getLoggedInState } = useContext(userContext);
+  const history = useHistory();
+  useEffect(() => {
+    getLoggedInState();
+    return () => {
+      <></>;
+    };
+  }, [getLoggedInState]);
+  return (
+    <div className='container-fluid' style={{ color: "rgb(97, 15, 89)" }}>
+      {loggedIn ? (
+        <>
+          <ShortnerForm />
+        </>
+      ) : (
+        <>
+          <button
+            onClick={() => history.push("/login")}
+            className='btn btn-success'
+          >
+            Login to continue
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Shortner;
